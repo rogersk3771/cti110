bb1b96e8120d2cc06b2ab336a36cf348f7b9c253
rogersk3771 <40347871+rogersk3771@users.noreply.github.com>
2018-06-17 23:35:58 -0400

    Add files via upload

---
+++ P2T1_SalesPrediction_RogersKenneth.py
@@ -0,0 +1,14 @@
+# CTI-110
+# P2T1 - Sales Prediction
+# Kenneth Rogers
+# June 14,2018
+#
+#Get the projected total sales
+total_sales = float(input('Enter the projected sales: '))
+
+# Calculate the profite as 20 percent of total sales.
+profit = total_sales * 0.20
+
+# Display the profit.
+print('The profit is $', format(profit, ',.2f'))
+
