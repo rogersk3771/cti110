bb43e5c2352c0141620cc5847041758f38ee252b
rogersk3771 <40347871+rogersk3771@users.noreply.github.com>
2018-06-22 14:06:23 -0400

    Add files via upload

---
+++ P3T1_AreasOfRectangles_KennethRogers.py
@@ -0,0 +1,24 @@
+# CTI-110
+# P3T1 - Area of Rectangles 
+# Kenneth Rogers
+# June 22,2018
+
+# Get the dimensions of rectangle1.
+length1 = int(input('Enter the length of rectangle 1: '))
+width1 = int(input('Enter the width of rectangle 1: '))
+
+# Get the dimensions of rectangle2.
+length2 = int(input('Enter the length of rectangle 2: '))
+width2 = int(input('Enter the width of rectangle 2: '))
+
+#Calculate the areas of the rectangles.
+area1 = length1 * width1
+area2 = length2 * width2
+
+# Determine which rectangle has the greatest area.
+if area1 > area2:
+    print('Rectangle 1 has the greater area.')
+elif area2 > area1:
+    print('Rectangle 2 has the greater area.')
+else:
+    print('Both have the same area.')
